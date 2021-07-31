de3e57280d70a355077d2b5d71a94c60c665d8ad
MK MONI <kiranmonireddy@gmail.com>
2021-07-31 11:45:49 +0530

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -1,7 +1,5 @@
 /*Develop a C program for MegeSort*/ 
 #include<stdio.h>
-#include <time.h>
-#include <limits.h>
 void readArray(int [], int);
 void mergesort(int [], int, int);
 void merge(int [], int, int, int);
@@ -10,18 +8,14 @@ int i=0;
 void main()
 {
 	int a[50], n;
-	clock_t tstart,tend;
 	printf("Enter total number of elements you want to sort:  ");
 	scanf("%d",&n);
 	readArray(a, n);
 	printf("\nBefore Sorting: \n");
 	printarray(a,n);
-	tstart=clock(); 
 	mergesort(a,0,n-1);
-	tend=clock(); 
 	printf("\nAfter Sorting: \n");
 	printarray(a,n);
-	printf("\nTime taken in Merge Sort: %.6fs\n", ((double)(tend - tstart) )/ CLOCKS_PER_SEC);
 }
 void readArray(int a[], int n)
 {
